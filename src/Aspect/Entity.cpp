8fa381f9ae940c17efa1b0c265a9e899a4637650
Jamie Johnston <JJohnston_16@outlook.com>
2018-10-12 11:08:30 +0100

    Entity Push

---
+++ src/Aspect/Entity.cpp
@@ -0,0 +1,31 @@
+#include "Entity.h"
+#include <iostream>
+
+namespace Aspect
+{
+	namespace Engine
+	{
+
+		Entity::Entity()
+		{
+
+
+
+		}
+
+		Entity::~Entity()
+		{
+
+			// Trying to push
+		}
+
+		void Entity::addEntity()
+		{
+
+
+			// Template Type T?
+
+
+		}
+	}
+}
---
+++ src/Aspect/Entity.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace Aspect
+{
+	namespace Engine
+	{
+
+		class Entity
+		{
+		public:
+
+			Entity();
+			~Entity();
+
+
+			void addEntity();
+
+
+
+			// Trying to push it
+		};
+	}
+}
